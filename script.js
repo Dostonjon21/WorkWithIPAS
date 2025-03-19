1a3a3d0a691afb822ca0a72695b5d0df6744bb46
Doston12 <rajabovdost@gmail.com>
2025-03-19 17:27:56 +0500

    add

---
+++ script.js
@@ -0,0 +1,22 @@
+// let obj = {
+//     get propName() {
+//       // геттер, срабатывает при чтении obj.propName
+//     },
+  
+//     set propName(value) {
+//       // сеттер, срабатывает при записи obj.propName = value
+//     }
+//   };
+//   console.log(obj);
+  
+
+let user = {
+    name: "John",
+    surname: "Smith",
+  
+    get fullName() {
+      return `${this.name} ${this.surname}`;
+    }
+  };
+  
+  alert(user.fullName); // John Smith
